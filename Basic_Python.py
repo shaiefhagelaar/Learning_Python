d4adab90e0519ff0f0b282d72c5da99ab1c0b020
Elephant_On_Acid <162466929+shaiefhagelaar@users.noreply.github.com>
2026-03-20 17:16:27 +0100

    Update Basic_Python.py
    taking user input, store into variables and combine the two into a name

--- Basic_Python.py
+++ Basic_Python.py
@@ -1,9 +1,19 @@
 
-#Basic's
+#basic's
 print("Hello " + (name := input('Enter your name: ')) + "!, characters: " + str(len(name)))
 
-#Switching contents Variables
+#switching contents variables
 glass1 = "milk"
 glass2 = "juice"
 
 glass1, glass2 = glass2, glass1
+
+#taking user input, store into variables and combine the two into a name
+
+print ("Welcome to the Band Name Generator")
+
+location_lived = input("Where are you form?")
+
+pet_name = input("What is the name of your pet?")
+
+print("Your Band name is: " + location_lived + " " + pet_name)
