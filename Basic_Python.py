02442a4f61e45204dde19227fad4e9c07830c4a5
Elephant_On_Acid <162466929+shaiefhagelaar@users.noreply.github.com>
2026-03-20 16:33:01 +0100

    Update Basic_Python.py
    Switching contents Variables

--- Basic_Python.py
+++ Basic_Python.py
@@ -1,1 +1,10 @@
+
+#Basic's
 print("Hello " + (name := input('Enter your name: ')) + "!, characters: " + str(len(name)))
+
+#Switching contents Variables
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass1, glass2 = glass2, glass1
